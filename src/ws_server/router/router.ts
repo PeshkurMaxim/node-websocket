7b4a32f158a878efb5285b357945a417ce491cd1
= <=>
2023-01-21 14:08:47 +0300

    add draw event

--- src/ws_server/router/router.ts
+++ src/ws_server/router/router.ts
@@ -1,12 +1,17 @@
 import { Writable } from "stream";
+import * as drawEvents from "../services/draw";
 import * as mouseEvents from "../services/mouse";
 
 export const Router = async (message: String, response: Writable) => {
     const cmd = message.toString().split(" ");
     let result = null;
-        
+    
     if (isMouseEvent(cmd[0])) {
         result = await mouseEvents[cmd[0]](+cmd[1]);
+    }
+
+    if (isDrawEvent(cmd[0])) {
+        result = await drawEvents[cmd[0]](+cmd[1], +cmd[2]);
     }
 
     if (!result)
@@ -14,12 +19,21 @@ export const Router = async (message: String, response: Writable) => {
 
     response.write(result);
     
-    // ["draw_square", "draw_rectangle", "draw_circle"]
     // ["prnt_scrn"]
 }
 
 function isMouseEvent(str: String): str is MouseEvents {
     for (const key in MouseEvents) {
+        if (key == str) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isDrawEvent(str: String): str is DrawEvents {
+    for (const key in DrawEvents) {
         if (key == str) {
             return true;
         }
@@ -34,4 +48,10 @@ enum MouseEvents {
     mouse_left = "mouse_left",
     mouse_right = "mouse_right",
     mouse_position = "mouse_position"
+}
+
+enum DrawEvents {
+    draw_square = "draw_square",
+    draw_rectangle = "draw_rectangle",
+    draw_circle = "draw_circle"
 }
---
+++ src/ws_server/services/draw.ts
@@ -0,0 +1,39 @@
+import { mouse, left, up, down, right, Point, Button, straightTo } from "@nut-tree/nut-js";
+
+export const draw_circle = async (pixels: number) => {
+    const start = await mouse.getPosition();
+    const center = new Point(start.x, start.y + pixels);
+    const step = 0.01;
+
+    await mouse.pressButton(Button.LEFT);
+
+    for (let radius = 0; radius <= Math.PI * 2; radius += step) {
+        const y = pixels * Math.cos(radius);
+        const x = pixels * Math.sin(radius);
+        await mouse.move(straightTo(new Point(center.x + x, center.y - y)));
+    }
+
+    await mouse.releaseButton(Button.LEFT);
+}
+
+export const draw_square = async (pixels: number) => {
+    await drawRectangle(pixels, pixels);
+}
+
+export const draw_rectangle = async (pixelsX: number, pixelsY: number) => {
+    await drawRectangle(pixelsX, pixelsY);
+}
+
+const drawRectangle = async (x: number, y: number) => {
+    await mouse.pressButton(Button.LEFT);
+
+    await mouse.move(right(x));
+    await mouse.move(down(y));
+    await mouse.move(left(x));
+    await mouse.move(up(y));
+
+    await mouse.releaseButton(Button.LEFT);
+}
+
+
+
